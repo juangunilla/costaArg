d23831f87c371029f1f8e028e86bb07aab3ab2c4
juangunilla <gunillajuan@gmail.com>
2022-05-18 23:03:31 -0300

    Actualización de get clima

--- axio.js
+++ axio.js
@@ -1,16 +1,23 @@
 const ObtenerClima = async() => {
     try{
-      const respuesta = await axios.get('http://localhost:4567/api/climas')
+      const respuesta = await axios.get('http://localhost:3000/api/climas')
   
-      console.log(respuesta.data.docs);
+      console.log(respuesta.data);
   
       if(respuesta.status ===200){
         let ciudad ='';
-        respuesta.data.docs.forEach(ciudad => {
+        respuesta.data.data.forEach(ciudad => {
           ciudad = `<div class="card" > <h3 class="ciudad">${ciudad.Ciudad}</h3>
-          <img class="imgclima" src="http://localhost:8080/img/${ciudad.img}"> 
+          <img class="imgclima" src="${ciudad.img}"> 
           <p class="ciudad">${ciudad.Descripcion}</p> 
-          <h2 class="ciudad">°${ciudad.Temp}C<h2>
+          <p class="subti">Humedad Viento</p> 
+          <p class="viento">${ciudad.Humedad}%  ${ciudad.Viento}KM/H<p>
+          <p class="subti">Tmax Tmin</p> 
+          <p class="tM">${ciudad.tMax}°  ${ciudad.tMin}°<p>
+          <p class="subti">Sensación</p> 
+          <p class="sensacion">${ciudad.sT}°<p>
+
+          
           `;
           document.getElementById('contenedor').innerHTML += ciudad;
   
